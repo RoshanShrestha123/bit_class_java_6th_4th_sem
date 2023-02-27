addc68222f996be66b41399a0c0148bc22a1b264
Roshan Shrestha <roshan.shrestha4u@gmail.com>
2023-02-27 17:17:34 +0545

    youtube video link added based on the unit

--- src/VectorLearning.java
+++ src/VectorLearning.java
@@ -1,3 +1,5 @@
+//import java.util.Vector;
+
 import java.util.Vector;
 
 public class VectorLearning {
@@ -5,17 +7,17 @@ public class VectorLearning {
         // This is a fixed length array.
         int[] age = {10,20,30}; //length = 3;
 
-        Vector age2 = new Vector();
+        Vector age2 = new Vector(3,10);
         age2.add(10);
         age2.add(20);
         age2.add(30);
         age2.add(30);
-        age2.add(30);
-        age2.add(30);
-        age2.add(30);
-        age2.add(30);
-        age2.add(30);
-        age2.add(30);
+//        age2.add(30);
+//        age2.add(30);
+//        age2.add(30);
+//        age2.add(30);
+//        age2.add(30);
+//        age2.add(30);
         System.out.println(age2);
         System.out.println("Vector age capacity>> " +age2.capacity());
 
