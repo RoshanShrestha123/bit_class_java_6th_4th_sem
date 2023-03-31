c19da3db33d5afb16d83d1340cc2a8b3bb066421
tamanghawana0107 <hawanatamang@gmail.com>
2023-03-31 21:57:44 +0545

    Modified Code

--- src/Password Generator/Main.java
+++ src/Password Generator/Main.java
@@ -1,14 +1,14 @@
-// Simple java code to generate most strong password
+//Simple java code to generate most strong password
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args)
     {
         Random random = new Random();
-        String lc = "abcdefghijklmnopqrstwxyz";
+        String lc = "abcdefghijklmnopqrstuvwxyz";
         String uc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String number = "0123456789";
-        String symbols = " @#$%&*";
+        String symbols = "@#$%&*";
         String use = lc + uc + number + symbols;
         int length = 8;
         char[] pass = new char[length];
